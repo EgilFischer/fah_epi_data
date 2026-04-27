df0ae8d6188b46d13b453f8f559bd4a72d9d5934
Egil Fischer <e.a.j.fischer@uu.nl>
2026-04-27 10:10:24 +0200

    Added LPAI data

--- R/load_fahepi_data.R
+++ R/load_fahepi_data.R
@@ -5,3 +5,6 @@ rendac_data <- read.table("data-raw/Rendac.txt", sep ="\t", header = TRUE)
 
 usethis::use_data(rendac_data, overwrite = TRUE)
 
+lpai_data <- read.csv("data-raw/dataset_LPAI_R.csv")
+
+usethis::use_data(lpai_data, overwrite = TRUE)
